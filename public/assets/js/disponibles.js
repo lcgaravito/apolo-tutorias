e6a6ba5ea5c5cc860243e4ae2c46cae5b73a2878
ar-corinaldi <47058600+ar-corinaldi@users.noreply.github.com>
2020-03-03 06:31:50 -0500

    Documentation and use of strict

--- public/assets/js/disponibles.js
+++ public/assets/js/disponibles.js
@@ -1,8 +1,11 @@
+"use strict";
+//Fetch to get info from the server in /categorias
 fetch('/categorias')
     .then(function(res) {
         return res.json();
     })
     .then(function(data) {
+        //Add information to the HTML 
         let result = '';
         data.forEach((category) => {
             result += `<a class="btn btn-outline-primary btn-lg" href="/disponibles/${category}" role="button">${category}</a>`;
